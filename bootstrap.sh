18d88c4b6d80c2e07103a0035db7dca36056f0b8
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2017-07-29 14:45:32 -0400

    Added .profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,4 +12,9 @@ cp ./dots/.*[a-zA-Z]* ~
 
 rm -rf ~/Public ~/Music ~/Videos ~/Templates ~/Documents
 
+echo "PS1=\"\[\033[38;5;3m\]\W \[\033[38;5;15m\]> \"" >> ~/.profile
+
+echo "set completion-ignore-case on" >> ~/.inputrc
+echo "set bell-style none" >> ~/.inputrc
+
 /bin/bash
