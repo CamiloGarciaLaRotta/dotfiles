e1954d0913d513f97fe79a2af3ddf0c21748d92c
Camilo Garcia La Rotta <camilogarcialarotta@github.com>
2021-07-23 08:32:38 -0400

    feat: split between linux & mac setup

--- install.sh
+++ install.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-exec > >(tee -i $HOME/dotfiles_install.log)
+exec > >(tee -i "$HOME/dotfiles_install.log")
 exec 2>&1
 set -x
 
 echo "fooooo setup.sh"
 
-# codespaces won't need rbenv if $CODESPACES is defined no need for it
-# brew install  on unix based on the system it's being run on
-# dont make symlinks from ./dots but from pwd
-if ! [ "$CODESPACES" = true ]; then
-   echo "WAKAFLAKA"
-fi
 vim_path="$HOME/.vim/pack/plugins/start"
 
-function brewget {
+function brew_get {
   if brew ls --versions "$1"; then
     brew upgrade "$1"
   else
@@ -35,24 +29,57 @@ function clone {
   fi
 }
 
-if which -s brew; then
-  echo "=> updating brew"
-  brew update
+function setup_brew {
+  if which brew; then
+    echo "=> updating brew"
+    brew update
+  else
+    echo "=> installing brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+}
+
+if [ "$CODESPACES" = true ]; then
+  echo "WAKAFLAKA"
+
+  # remove existing init scripts
+  rm -f "$HOME/.zshrc"
+  rm -f "$HOME/.gitconfig"
+
+  apt-get install -y \
+    bat exa locate ripgrep shellcheck tmux zsh-autosuggestions
+
+  # Setup the database for locate
+  updatedb
 else
-  echo "=> installing brew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo "NO WAKAFLAKA"
+  setup_brew
+
+  brew_get "bat"
+  brew_get "exa"
+  brew_get "ripgrep"
+  brew_get "shellcheck"
+  brew_get "tmux"
+  brew_get "zsh"
+  brew-get "zsh-autosuggestions"
+
 fi
+
+chsh -s "$(which zsh)"
 
 # symbolic link for all dotfiles
 echo "=> symbolic link for dotfiles"
 find ./dots -maxdepth 1 -mindepth 1 -exec sh -c 'ln -s "$1" ~' sh {} \;
 
+rm -rf "$HOME/.fzf"
+git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+"$HOME/.fzf/install" --all
+mv "$HOME/.fzf/bin/fzf" "$BIN_PATH"
+
+
 # vim
 echo "=> configuring Vim"
 mkdir -p "$vim_path"
-
-brewget "fzf"
-brewget "tmux"
 
 clone "ap/vim-css-color"
 clone "dense-analysis/ale"
@@ -74,14 +101,7 @@ vim -u NONE -c "helptags vim-rhubarb/doc" -c q
 clone "vim-airline/vim-airline"
 vim -u NONE -c "helptags vim-airline/doc" -c q
 
-if [ "$SHELL" != "/usr/bin/zsh" ]; then
-  echo "=> installing zsh"
-  brew install zsh
-fi
-
 # To install useful key bindings and fuzzy completion:
 echo  "=> PLEASE RUN:"
-echo "$(brew --prefix)/opt/fzf/install"
 echo "echo \"machine api.github.com login <user> password <token>\" >> ~/.netrc"
 
-zsh
