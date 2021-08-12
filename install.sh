a31a8bffbbc1213fd57e33499fb2c973cd0c8b9a
Camilo Garcia La Rotta <camilogarcialarotta@github.com>
2021-08-12 10:20:35 -0400

    fix: no prompt when updating deb file

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ if [ "$CODESPACES" = true ]; then
     bat ripgrep shellcheck tmux zsh zsh-autosuggestions
 
   curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee -a /etc/apt/sources.list.d/github-cli.list > /dev/null
   sudo apt update
   sudo apt install gh
 else
